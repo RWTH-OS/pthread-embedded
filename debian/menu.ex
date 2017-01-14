47eeeb1b64f5f1e30ef8ad1e17b9b23ba50167a3
Daniel Krebs <github@daniel-krebs.net>
2017-01-14 23:29:48 +0100

    deb: delete unused files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pthread-embedded):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pthread-embedded" command="/usr/bin/pthread-embedded"
--- debian/pthread-embedded.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pthread-embedded package
-#
-0 4	* * *	root	[ -x /usr/bin/pthread-embedded_maintenance ] && /usr/bin/pthread-embedded_maintenance
